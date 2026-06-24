ad997c191260f4425ba7380ca1ab9ce2f071354b
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-06-24 11:42:39 +0700

    chore(vite): setup auto import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import AutoImport from 'unplugin-auto-import/vite';
 import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
@@ -17,10 +18,58 @@ export default defineConfig(({ mode }) => {
         },
 
         plugins: [
-            // https://react.dev/
+            // https://github.com/react/react
             react(),
 
-            // https://tailwindcss.com/
+            // https://github.com/unplugin/unplugin-auto-import
+            AutoImport({
+                include: [
+                    /\.[jt]sx?$/,
+                ],
+                imports: [
+                    'react',
+                    'react-router-dom',
+                    'react-i18next',
+                    {
+                        'zod': ['string', 'number', 'boolean', 'object', 'array', 'union', 'literal', ['enum', 'zEnum'], 'date', 'unknown', 'any', 'nullable', 'undefined', ['void', 'zVoid'], 'never'],
+                        'axios': [['default', 'axiosDefault'], 'axios'],
+                        '@tanstack/react-query': ['useQuery', 'useMutation', 'useInfiniteQuery', 'useQueries', 'useQueryClient'],
+                        'react-hook-form': ['useForm', 'Controller', 'useController'],
+                        '@hookform/resolvers/zod': ['zodResolver'],
+                        'lodash-es': ['debounce', 'throttle'],
+                        'clsx': ['clsx'],
+                        'class-variance-authority': ['cva'],
+                        'tailwind-merge': ['twMerge'],
+                        'zustand': ['create'],
+                        'date-fns': ['format', 'parseISO', 'addDays', 'subDays'],
+                    },
+                ],
+                dts: 'auto-import.d.ts',
+                dirs: [
+                    'src/common/builders/**',
+                    'src/common/hooks/**',
+                    'src/common/constants/**',
+                    'src/common/types/**',
+                    'src/common/endpoints/**',
+                    'src/common/exceptions/**',
+                    'src/common/stores/**',
+                    'src/common/services/**',
+                    'src/common/utils/**',
+
+                    'src/features/**/builders/**',
+                    'src/features/**/hooks/**',
+                    'src/features/**/constants/**',
+                    'src/features/**/types/**',
+                    'src/features/**/endpoints/**',
+                    'src/features/**/exceptions/**',
+                    'src/features/**/stores/**',
+                    'src/features/**/services/**',
+                    'src/features/**/utils/**',
+                ],
+                vueTemplate: false,
+            }),
+
+            // https://github.com/tailwindlabs/tailwindcss
             tailwindcss(),
         ],
 
