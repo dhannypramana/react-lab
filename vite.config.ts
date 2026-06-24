9eedb5e7606a86cdf2b5414b3acbc28ff1f5de96
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-06-24 10:58:20 +0700

    chore(env): setup env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+    const env = { ...process.env, ...loadEnv(mode, `${process.cwd()}/environments`, '') };
+    const port = !Number.isNaN(Number(env.VITE_APP_PORT)) ? Number(env.VITE_APP_PORT) : 5173;
+
     return {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@root': path.resolve(__dirname),
+            },
+        },
+
         plugins: [
             // https://react.dev/
             react(),
         ],
 
         server: {
-            port: 8000,
+            port,
+            strictPort: true,
         },
     };
 });
