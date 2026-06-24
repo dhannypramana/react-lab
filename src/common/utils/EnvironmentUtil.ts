1f6826781202335d9f7b0fce01115233b0504c1f
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-06-24 14:51:23 +0700

    fix(util): set VITE_APP_MODE into enum

--- src/common/utils/EnvironmentUtil.ts
+++ src/common/utils/EnvironmentUtil.ts
@@ -1,4 +1,4 @@
 export const env = object({
-    VITE_APP_MODE: string(),
+    VITE_APP_MODE: zEnum(['development', 'staging', 'production']),
     VITE_APP_PORT: string().transform(val => Number(val)),
 }).parse(import.meta.env);
